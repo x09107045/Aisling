4293b211469efcd5eefdf911847cd6728ea68fd7
x09107045 <aisling.roche@student.ncirl.ie>
2014-03-27 18:18:27 +0000

    TopNosh

---
+++ js/divsize.js
@@ -0,0 +1,25 @@
+onload=function(){
+	document.getElementById('nav-bar').style.height = document.getElementById('t-pcontent').offsetHeight + "px";
+}
+
+
+function validateForm(){
+	//var retType = false
+	//var test = document.getElementById("firstname");
+	//alert(test.value);	
+	//return retType;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
